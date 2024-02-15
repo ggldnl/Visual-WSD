0d82c5e4820bc65b8087cc61d90ad34200db287b
ggldnl <danielgigliotti99.dg@gmail.com>
2024-02-15 18:32:12 +0100

    Bug fixes

--- functions.py
+++ functions.py
@@ -3,17 +3,29 @@ import torch
 
 
 def contrastive_loss(logits, dim):
+    # Contrastive loss helps the model learn to pull similar images closer
+    # in the embedding space while pushing dissimilar images farther apart.
+    #
+    # Say we have 3 data points pairs (img_point_1, text_point_1),
+    # (img_point_2, text_point_2), (img_point_3, text_point_3) (a batch)
+    # We compute the similarity scores for each img_point with respect to each
+    # text_point. The similarity scores of the original pairs will be on the
+    # diagonal. We take the log_softmax converts the logits to probabilities
+    # (they sum up to 1). The mean returns a single loss value that is
+    # representing the average negative log probability of the correct classes
+    # across all pairs of data points. It is negative since we usually want to
+    # minimize this value.
     neg_ce = torch.diag(F.log_softmax(logits, dim=dim))
     return -neg_ce.mean()
 
 
 def clip_loss(sim):
     caption_loss = contrastive_loss(sim, dim=0)
-    image_loss = contrastive_loss(sim, dim=0)
+    image_loss = contrastive_loss(sim, dim=1)
     return (caption_loss + image_loss) / 2.0
 
 
-def similarity(v1, v2):
+def cosine_similarity(v1, v2):
 
     # Normalize the embeddings
     embedding1_normalized = F.normalize(v1, p=2, dim=-1)
@@ -23,9 +35,11 @@ def similarity(v1, v2):
     return F.cosine_similarity(embedding1_normalized, embedding2_normalized, dim=-1)
 
 
-def metrics(sim):
+def similarity_matrix(v1, v2):
+    return v1 @ v2.T
 
-    # TODO
+
+def metrics(sim):
 
     y = torch.arange(len(sim))
     img2cap_match_idx = sim.argmax(dim=1)
